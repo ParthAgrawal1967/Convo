b2dec7a0f42a6b255e4d07341d3724b27fb07f02
parthagrawal1967 <parthagrawal1967@gmail.com>
2026-01-20 03:12:49 +0530

    activated stun server

---
+++ frontend/src/pages/VideoMeet.jsx
@@ -0,0 +1,56 @@
+import React, { useRef, useState } from 'react'
+import "../styles/VideoComponent"
+const server_url="http://localhost:8000"
+
+var connections={}
+
+const peerConfigConnections={
+    "iceServers":[
+        {"urls":"stun:stun.l.google.com:19302"}
+    ]
+}
+
+export default function VideoMeetComponent() {
+
+    var socketRef=useRef();
+
+    let socketIdRef=useRef();
+
+    let localVideoRef=useRef();
+
+    let [videoAvailable, setVideoAvailable]=useState(true);
+
+    let [audioAvailable, setAudioAvailable]=useState(true);
+
+    let [video, setVideo]=useState();
+
+    let [audio, setAudio]=useState();
+
+    let [screen, setScreen]=useState();
+
+    let [showModal, setModal]=useState();
+
+    let [screenAvailable, setScreenAvailable]=useState();
+
+    let [messages, setMessages]=useState([]);
+
+    let [message, setMessage]=useState();
+
+    let [newMesages, setNewMessages]=useState(0);
+
+    let [askForUsername, setAskForUsername]=useState(true);
+
+    let [username, setUsername]= useState("");
+
+    const videoRef = useRef([]);
+
+    let [videos, setVideos] = useState([]);
+
+  return (
+    <div>
+        {askForUsername===true?
+        <div></div>:<></>    
+    }
+    </div>
+  )
+}
